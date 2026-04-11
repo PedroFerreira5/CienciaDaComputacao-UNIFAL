0dbe9da02785334f6530a228afd50ef21f77677b
Pedro <pedroferreira.prado@sou.unifal-mg.edu.br>
2026-04-11 11:17:12 -0300

    Add family relationship predicates and definitions in hello.pl

--- Programacao-Logica/hello.pl
+++ Programacao-Logica/hello.pl
@@ -1,1 +1,64 @@
-main :- write('Hello, world!'), nl.
+homem(joao).
+homem(alberto).
+homem(lucas).
+homem(pedro).
+homem(leandro).
+homem(leonardo).
+homem(julio).
+homem(eduardo).
+
+mulher(maria).
+mulher(claudia).
+mulher(regina).
+mulher(jasmine).
+mulher(julia).
+mulher(alice).
+
+pai(joao, claudia).
+pai(joao, alberto).
+pai(pedro, lucas).
+pai(lucas, leandro).
+pai(lucas, leonardo).
+pai(julio, jasmine).
+pai(alberto, eduardo).
+pai(alberto, alice).
+
+mae(maria, claudia).
+mae(maria, alberto).
+mae(regina, lucas).
+mae(claudia, leonardo).
+mae(claudia, leandro).
+mae(julia, jasmine).
+mae(jasmine, eduardo).
+mae(jasmine, alice).
+
+casado(joao, maria).
+casado(lucas, claudia).
+casado(julio, julia).
+casado(pedro, regina).
+casado(alberto, jasmine).
+
+irmao(X, Y) :-
+    genitor(G, X),
+    genitor(G, Y),
+    X \== Y.
+
+% 1. Criamos uma regra auxiliar: "P é genitor de X se P é pai OU P é mãe"
+genitor(P, X) :- pai(P, X).
+genitor(P, X) :- mae(P, X).
+
+% 2. Agora a regra de primo fica completa e simples:
+primo(X, Y) :-
+    genitor(G1, X),    
+    genitor(G2, Y),   
+    irmao(G1, G2),    
+    X \== Y.         
+
+
+filho(X, P, M) :- 
+    pai(P, X), 
+    mae(M, X).
+
+
+
+
