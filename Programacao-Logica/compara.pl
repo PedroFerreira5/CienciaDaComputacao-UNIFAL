83be4f14896ab8e5e5c1a01217ea7d354bf380e0
Pedro <pedroferreira.prado@sou.unifal-mg.edu.br>
2026-04-13 18:42:10 -0300

    Add compara function for list comparison in Prolog

---
+++ Programacao-Logica/compara.pl
@@ -0,0 +1,5 @@
+% consult('c:/Users/pedro/Documents/Faculdade/CienciaDaComputacao-UNIFAL/Programacao-Logica/compara.pl').
+compara([], []). %lista vazia
+
+% Passo Recursivo: Se as duas listas têm pelo menos um elemento,
+compara([_|Resto1], [_|Resto2]) :- compara(Resto1, Resto2).
