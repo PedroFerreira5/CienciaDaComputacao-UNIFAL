4032235e27a9d0366a51e3dc3059b0e1a23af36e
Pedro <pedroferreira.prado@sou.unifal-mg.edu.br>
2026-06-19 11:37:41 -0300

    Adicionar novas funções e ajustes de formatação no arquivo lista2.hs

--- Programacao-Funcional/lista2.hs
+++ Programacao-Funcional/lista2.hs
@@ -1,6 +1,9 @@
 {- HLINT ignore "Use foldr" -}
 {- HLINT ignore "Redundant list comprehension" -}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 import Prelude
+import Data.Char
+import Distribution.Compat.Lens (_1)
 
 -- 1
 soma :: [Int] -> Int
@@ -38,3 +41,37 @@ positions x xs = find x (zip xs [0..n])
 find :: Eq a => a -> [(a, Int)] -> [Int]
 find x pairs = [i | (a, i) <- pairs, a == x]
 
+-- 7 
+scalarproduct :: [Int]-> [Int]-> Int
+scalarproduct [] [] = 0
+scalarproduct as bs = soma [ a*b | (a,b) <- myZip as bs ] -- (produto cartesiano) [ (a,b) | a <- as, b <- bs ] todos com todos
+                                                       -- posição com posição [ a*b | (a,b) <- zip as bs ] 
+
+myZip :: [a] -> [b] -> [(a,b)]
+myZip (x:xs) (y:ys) = (x,y) : myZip xs ys
+myZip _ _ = []
+
+-- 8 
+infixr 8 &!
+(&!) :: Int -> Int -> Int  
+(&!) x 0 = 1                                -- ou usa (&!) só no parametro e coloca entre x e 0 (x &! 0) ou (&! x 0)
+(&!) x n | n > 0 = x * (x &! (n - 1))
+
+-- 9
+-- Main> [(+7) x | x <- [1..10], odd x].
+f9 :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+f9 f p xs = [ f x | x <- xs, p x ]
+
+f9b :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+f9b f p xs = map f (filter p xs)
+
+-- 10 
+dec2int :: [Int] -> Int
+dec2int [] = 0
+dec2int (d:ds) = d * 10 ^ myLength ds + dec2int ds
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (a:as) = 1 + myLength as 
+
+-- 11
